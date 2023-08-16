49df93b0bbac719e81e8a11c617d944ac04e8915
Crowdbotics <team@crowdbotics.com>
2023-08-16 14:51:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "books-hub-42875",
+  url: "https://books-hub-42875.botics.co",
+  api: "https://books-hub-42875.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
