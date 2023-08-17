70ac8cdee188db614263c31c8a632058218dea59
Saad bin Abid <saad.abid@crowdbotics.com>
2023-08-17 17:08:28 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI3/index.js
+++ screens/ScreenAI3/index.js
@@ -3,16 +3,29 @@ import { View, Text, Image, SafeAreaView, StyleSheet } from "react-native";
 
 const BookScreen = () => {
   return <SafeAreaView style={styles.container}>
+      
       <View style={styles.bookContainer}>
+        <View style={styles.header}>
         <Image source={{
-        uri: "https://tinyurl.com/42evm3m3"
-      }} style={styles.bookImage} />
+          uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShFEmS3xQVIFPFh-JPkxXOuY059M3RGTwdkg&usqp=CAU"
+        }} style={styles.backIcon} />
+        <Text style={styles.headerText}>Book Title</Text>
+      </View>
+        <View style={styles.jIjDQRcf}>
+         <Image source={{
+          uri: "https://tinyurl.com/42evm3m3"
+        }} style={styles.bookImage} />
         <View style={styles.bookInfoContainer}>
           <Text style={styles.publisherName}>Publisher Name</Text>
           <Text style={styles.publishingDate}>Publishing Date</Text>
           <Text style={styles.bookReviews}>Book Reviews</Text>
-          <Text style={styles.bookDescription}>Book Description</Text>
         </View>
+        </View>
+       
+
+          <Text style={styles.bookDescription}> Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</Text>
+        
+        
       </View>
     </SafeAreaView>;
 };
@@ -24,8 +37,23 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center"
   },
-  bookContainer: {
+  header: {
     flexDirection: "row",
+    backgroundColor: "white",
+    padding: 20,
+    width: '90%'
+  },
+  backIcon: {
+    width: 20,
+    height: 20,
+    marginRight: 10
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 70
+  },
+  bookContainer: {
     alignItems: "center",
     backgroundColor: "white",
     borderRadius: 10,
@@ -64,7 +92,12 @@ const styles = StyleSheet.create({
   },
   bookDescription: {
     fontSize: 16,
-    color: "#666666"
+    color: "#666666",
+    textAlign: 'center',
+    marginTop: 30
+  },
+  jIjDQRcf: {
+    flexDirection: "row"
   }
 });
 export default BookScreen;
